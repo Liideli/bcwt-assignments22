fd549b45a370e91bb7564ecfc4d0aeb2823e56a2
Liideli <roope.kasperi.laine@gmail.com>
2022-10-26 14:58:19 +0300

    add static files public folder

---
+++ server-one/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get("/catinfo", (req, res) => {
+    const cat = {
+      name: "Frank",
+      birthdate: "2010-12-25",
+      weight: 5,
+    };
+    res.json(cat);
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
+app.use(express.static('public'));

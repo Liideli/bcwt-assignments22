cc3da70d9d9e9631d6c2d0562c5c82697b463895
Liideli <roope.kasperi.laine@gmail.com>
2022-10-26 13:32:07 +0300

    added nodemon devDependency, ex3 done

--- week1/script.js
+++ week1/script.js
@@ -2,11 +2,8 @@
 
 const _ = require('lodash');
 
-_.camelCase
-
 // Ex. 1
 console.log('Hello World, I am Node');
-console.log('Hello World, I am Node'); 
 
 // Ex. 2
 let output = 'Just Testing nodemon, using lodash to convert to camelcase';

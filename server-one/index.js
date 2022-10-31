a6c98192a53b7f18555a775bac405ebb0c3e0ab2
Liideli <roope.kasperi.laine@gmail.com>
2022-10-31 19:42:49 +0200

    added server-two for testing pug

--- server-one/index.js
+++ server-one/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let requestCounter = 0;
+
 
 app.get("/catinfo", (req, res) => {
     const cat = {
@@ -14,8 +16,21 @@ app.get("/catinfo", (req, res) => {
     res.json(cat);
   });
 
+app.get("/test", (request, response) => {
+    console.log('Someone is trying to test me');
+    requestCounter;
+    // Example using pug
+    response.render('test', {
+    title: "Pug test page",
+    header1: "Pug test page",
+    header2: "Counter",
+    exampleText: "PAge requested " + requestCounter + "times."
+    });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
 
 app.use(express.static('public'));
+app.set('view engine', 'pug');

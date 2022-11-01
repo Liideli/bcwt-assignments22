038f32739dd8cc0d017159adf4eb6650d5a5f299
Liideli <roope.kasperi.laine@gmail.com>
2022-11-01 19:03:25 +0200

    added catRouter for /cat

--- week-two/app.js
+++ week-two/app.js
@@ -1,35 +1,15 @@
 'use strict';
 const express = require('express');
 const app = express();
+const catRouter = require('./routes/catRoute');
 const port = 3000;
 
-app.get('/cat', (req, res) => {
-  //console.log(req);
-  res.send('From this endpoint you can get cats.');
-});
-
-app.get('/cat/:catId', (req, res) => {
-  //console.log(req.params);
-  res.send('From this endpoint you can get cat with id ' + req.params.catId);
-});
+app.use('/cat', catRouter);
 
 app.get('/user', (req, res) => {
   res.send('From this endpoint you can get users.');
 });
 
-app.post('/cat', (req, res) => {
-  //console.log(req);
-  res.send('From this endpoint you can add more cats');
-});
-
-app.put('/cat', (res, req) => {
-  //console.log(req);
-  res.send('From this endpoint you can replace cats');
-}); 
-
-app.delete('/cat', (res, req) => {
-  //console.log(req);
-  res.send('From this endpoint you can delete cats');
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
